956b77d2ed0fcbba908fdde117151cf76fed685d
nurlan1986 <vozdushnyi.pirat06@gmail.com>
2021-06-12 23:20:09 +0600

    avatar.jpg

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "vytfgbhnjmk", Toast.LENGTH_SHORT).show();
     }
 
 }
